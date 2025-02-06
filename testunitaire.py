26772662ee2d969378215870d51b605ad1c0e60b
Martin PREVOT <martin.prevot@efrei.net>
2025-02-06 09:54:14 +0100

    ajout de test

---
+++ testunitaire.py
@@ -0,0 +1,23 @@
+import unittest
+from io import StringIO
+import sys
+from main import fizzbuzz
+
+class TestFizzBuzz(unittest.TestCase):
+    def test_fizzbuzz(self):
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        fizzbuzz(15)
+
+        sys.stdout = sys.stdout
+        output = captured_output.getvalue().splitlines()
+
+        expected_output = [
+            "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz",
+            "11", "Fizz", "13", "14", "FizzBuzz"
+        ]
+        self.assertEqual(output, expected_output)
+
+if __name__ == 'main':
+    unittest.main()
